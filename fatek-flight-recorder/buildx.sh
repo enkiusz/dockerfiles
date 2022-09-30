b80182be79621968e266413d04a30442ef8a48e5
Maciej Grela <enki@fsck.pl>
2022-09-30 17:43:26 +0200

    fatek-flight-recorder: Import all USB ports, build all architectures

--- fatek-flight-recorder/buildx.sh
+++ fatek-flight-recorder/buildx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#docker buildx build --push --platform linux/arm64/v8,linux/i386,linux/amd64 --tag enkiusz/fatek-flight-recorder:latest .
-docker buildx build --push --platform linux/amd64 --tag enkiusz/fatek-flight-recorder:latest .
+docker buildx build --push --platform linux/arm64/v8,linux/i386,linux/amd64 --tag enkiusz/fatek-flight-recorder:latest .
+
