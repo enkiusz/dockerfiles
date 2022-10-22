607833ba540a44e8b016f0fafe937352f3416882
Maciej Grela <enki@fsck.pl>
2022-10-22 20:49:23 +0200

    cockroachdb: Initial revision

---
+++ cockroachdb/buildx.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#docker buildx build --push --platform linux/arm64/v8,linux/i386,linux/amd64 --tag enkiusz/cockroachdb:latest .
+docker buildx build --push --platform linux/arm64/v8,linux/i386,linux/amd64 --tag enkiusz/cockroach:latest .
